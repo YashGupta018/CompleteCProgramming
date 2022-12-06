bac350b732e790c4244baaae8ae40f7c56f660a2
YashGupta018 <ygupta2001@gmail.com>
2022-12-06 16:01:05 +0530

    comments in c

---
+++ Comments/comments.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    //This is single line comment
+
+    printf("This is a comment //This is single line comment\n");
+
+    /*
+    This 
+    is 
+    multi 
+    line 
+    comment
+    */
+    
+    printf("This is a comment /*This is multi line comment*/\n");
+
+    return 0;
+}
