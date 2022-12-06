0fceb8a649df6c5775f46ea3dbe31a500a9b67ae
YashGupta018 <ygupta2001@gmail.com>
2022-12-06 12:40:51 +0530

    using different data types

---
+++ 3. Data Types/dataTypes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int age = 42;
+    double sgpa = 8.9;
+    char grade = 'A';
+    char phrase[] = "Hi guy";
+
+    printf("Age : %d \n", age);
+    printf("SGPA : %f \n", sgpa);
+    printf("Grades : %c \n", grade);
+    printf("Phrase : %s \n", phrase);
+
+    return 0;
+}
