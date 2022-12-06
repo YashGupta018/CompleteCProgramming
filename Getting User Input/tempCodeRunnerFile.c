de217db60a3cb3c5fbce28558ccbf93f95099fba
YashGupta018 <ygupta2001@gmail.com>
2022-12-06 19:20:58 +0530

    basics of getting input from user

---
+++ Getting User Input/tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+int age;
+    // printf("Enter you age : ");
+    // scanf("%d", &age); //& this is a pointer
+    // printf("The age of the user is : %d\n", age);
+    // printf("\n");
+
+    // double sgpa;
+    // printf("Enter you sgpa : ");
+    // scanf("%lf", &sgpa); //& this is a pointer
+    // printf("The sgpa of the user is : %f\n", sgpa);
+    // printf("\n");
+
+    // char grade;
+    // printf("Enter you grade : ");
+    // scanf("%s", &grade); //& this is a pointer
+    // printf("The grade of the user is : %c\n", grade);
+    // printf("\n");
+
+    // char name[20];
+    // printf("Enter you full name : ");
+    // scanf("%s", name); //in this case we dont need &
+    // printf("The name of the user is : %s\n", name);
+    // printf("\n");
