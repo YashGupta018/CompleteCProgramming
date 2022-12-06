b264e24812c92905504c265efc0fd9cd35ddc61b
YashGupta018 <ygupta2001@gmail.com>
2022-12-06 15:33:19 +0530

    using printf efficiently

---
+++ Printf/printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int dob = 1301;
+
+    /*
+    float %f
+    char(string) %s
+    double %f
+    char %c
+    int %d
+    */
+
+    printf("Hello \"World\"\n");
+    printf("My fav %s is %d & %f %s %d", "number", 500, 234.43 , "and my dob is", dob);
+
+
+
+    return 0;
+}
