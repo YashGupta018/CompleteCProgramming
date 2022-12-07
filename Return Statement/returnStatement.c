688454df6d3b8c19082f9201453559c9208c02d4
YashGupta018 <ygupta2001@gmail.com>
2022-12-07 15:24:21 +0530

    Functions in C

---
+++ Return Statement/returnStatement.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double cube(double num);
+
+// double cube(double num) {
+//     //double result = num * num * num;
+//     return /*result =*/ num * num * num;
+// }
+
+int main() {
+
+    printf("%f\n", cube(3.0));
+
+    return 0;
+}
+
+// double cube(double num) {
+//     double result = num * num * num;
+//     return result = num * num * num;
+// }
+
+double cube(double num) {
+    //double result = num * num * num;
+    return /*result =*/ num * num * num;
+}
---
+++ Return Statement/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+double cube(double num){
+//     double result = num * num * num;
+//     return result = num * num * num;
+// }
